339576910c426415bb81271d9a494b025f735b60
Victor <laivictor80@gmail.com>
2023-01-12 23:06:36 -0800

    added html css js

---
+++ script.js
@@ -0,0 +1,35 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1-num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(operator, num1, num2) {
+    if (operator=="+") {
+        return add(num1, num2);
+    }
+    else if(operator=="-") {
+        return subtract(num1, num2);
+    }
+    else if(operator=="*") {
+        return multiply(num1, num2);
+    }
+    else if(operator=="/") {
+        return divide(num1, num2);
+    }
+}
+
+let num = document.querySelector(".num");
+num.addEventListener("click", () => {
+    console.log(num);
+})
